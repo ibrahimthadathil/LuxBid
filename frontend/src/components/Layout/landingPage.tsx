1512b710aa70fe2259796a36b2ff86155bb1e3ec
ibrahim <ibrahiminfo8@gmail.com>
2025-06-01 23:21:48 +0530

    changed the home text color

--- frontend/src/components/Layout/landingPage.tsx
+++ frontend/src/components/Layout/landingPage.tsx
@@ -7,19 +7,19 @@ const LandingPage = () => {
       <div className="max-w-3xl mx-auto">
         <div className="mb-6 sm:mb-8">
           <p className="text-xs sm:text-sm text-gray-400">
-            Our Capital, Your <span className="text-purple-400">Success</span>
+            Our Capital, Your <span className="text-[#5135d1ea]">Success</span>
           </p>
           <h1 className="mt-2 text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
             <span className="text-white">No Time Limit Prop </span>
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-purple-600">Firm</span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-white via-[#3b3365ea] to-[#2e2361ea]">Firm</span>
           </h1>
           <h2 className="mt-2 text-2xl sm:text-3xl md:text-4xl font-bold tracking-tight">
             <span className="text-white">Conquer The </span>
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-purple-600">market</span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-white via-[#3b3365ea] to-[#2e2361ea]">market</span>
           </h2>
         </div>
 
-        <Button className="mt-6 sm:mt-8 bg-purple-600 hover:bg-purple-700">
+        <Button className="mt-6 sm:mt-8 bg-white hover:bg-purple-700 hover:text-white">
           Let's start <ArrowRight className="ml-2 h-3 w-3 sm:h-4 sm:w-4" />
         </Button>
       </div>
